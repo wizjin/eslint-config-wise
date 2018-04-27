cd66e76b38530c61918b1c88055860f6caa57262
wizjin <wizjin@icloud.com>
2018-04-27 11:22:15 +0800

    Fix package index.js.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-module.exports = require('./lib').default;
+module.exports = {
+  "extends": "airbnb",
+  "rules": {
+    "spaced-comment": ["error", "always", { "line": { "exceptions": ["-", "/"] } }],
+    "no-unused-vars": ["error", { "vars": "all", "args": "after-used" }],
+    "no-constant-condition": [0],
+    "global-require": [0],
+    "comma-dangle": ["error", {
+        "functions": "never",
+        "imports": "only-multiline",
+        "arrays": "only-multiline",
+        "objects": "only-multiline"
+      }],
+    "jsx-a11y/href-no-hash": [0]
+  },
+  "plugins": [
+    "react"
+  ],
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true,
+      "modules": true
+    }
+  }
+}
