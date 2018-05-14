8cb615b5e7af56964ec6f59abcb45ebd94fd4e8f
wizjin <wizjin@icloud.com>
2018-05-14 18:04:41 +0800

    release 1.3.0

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ module.exports = {
   "extends": "airbnb-base",
   "rules": {
     "spaced-comment": ["error", "always", { "line": { "exceptions": ["-", "/"] } }],
+    "generator-star-spacing": ["error", {"before": true, "after": false}],
     "no-unused-vars": ["error", { "vars": "all", "args": "after-used" }],
+    "no-param-reassign": ["error", { "props": false }],
     "no-constant-condition": [0],
+    "object-curly-newline": [0],
+    "no-restricted-syntax": [0],
+    "import/no-unresolved": [0],
+    "import/extensions": [0],
     "global-require": [0],
     "comma-dangle": ["error", {
         "functions": "never",
